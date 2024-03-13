d12183432e113fac6f47087ebf113fa6db92159c
siddharth9442 <siddharthkhubikar9442@gmail.com>
2024-03-13 11:52:44 +0530

    Add subscription controller

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,8 +1,7 @@
 import { Router } from 'express'
-import { getVideoByTitle, uploadVideo } from '../controllers/Video.controller.js'
+import { deleteVideo, getAllVideos, getVideoById, publishAVideo, togglePublishStatus, updateVideo } from '../controllers/Video.controller.js'
 import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJWT } from '../middlewares/auth.middleware.js'
-import { updateViewsCount } from '../middlewares/views.middleware.js'
 
 const router = Router()
 
@@ -18,9 +17,31 @@ router.route('/upload-video').post(
             maxCount: 1
         }
     ]),
-    uploadVideo
+    publishAVideo
 )
 
-router.route('/:title').get(verifyJWT, updateViewsCount, getVideoByTitle)
+router.route('/all-videos').get(verifyJWT, getAllVideos)
+
+router.route('/:videoId').get(verifyJWT, getVideoById)
+
+router.route('/update-video/:videoId').patch(
+    verifyJWT,
+    upload.fields([
+        {
+            name: 'videoFile',
+            maxCount: 1,
+        },
+        {
+            name: 'thumbnail',
+            maxCount: 1
+        }
+    ]),
+    updateVideo
+)
+
+router.route('/delete-video/:videoId').delete(verifyJWT, deleteVideo)
+
+router.route('/toggle-publish/:videoId').put(verifyJWT, togglePublishStatus)
+
 
 export default router;
